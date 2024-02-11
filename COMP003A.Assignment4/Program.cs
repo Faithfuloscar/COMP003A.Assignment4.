fab42719e71ce7109d660f191fcc1a0910563644
oscar <oscar@LAPTOP-G1N9MG0D>
2024-02-10 22:19:06 -0800

    Add project files.

---
+++ COMP003A.Assignment4/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Author: Oscar Mendez
+ * Course: COMP-003A
+ * Purpose: to complete assignment as shown for loop statements
+ */
+
+namespace COMP003A.Assignment4_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
